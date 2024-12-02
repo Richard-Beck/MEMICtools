de2ac8231e43f648029c2e3c6f2d3138b22ec041
= <=>
2024-12-02 16:01:19 -0500

    updated metadata analysis

--- R/analyze_metadata.R
+++ R/analyze_metadata.R
@@ -9,35 +9,38 @@ x <- split(x,f=interaction(x$Row,x$Col,x$Channel,x$Timepoint))
 cl <- makeCluster(getOption("cl.cores", ncores))
 
 parLapplyLB(cl=cl,X=x,fun=function(xi){
-  dxy <- 0.001217608
-  nxy <- 2160  
-  id <- paste0("r",stringr::str_pad(xi$Row[1],2,pad=0),
-               "c",stringr::str_pad(xi$Col[1],2,pad=0),
-               "fxxpxx-",
-               "ch",xi$Channel[1],
-               "sk",xi$Timepoint[1],
-               "fk1fl1.tiff")
+  tryCatch({
+    dxy <- 0.001217608
+    nxy <- 2160  
+    id <- paste0("r",stringr::str_pad(xi$Row[1],2,pad=0),
+                 "c",stringr::str_pad(xi$Col[1],2,pad=0),
+                 "fxxpxx-",
+                 "ch",xi$Channel[1],
+                 "sk",xi$Timepoint[1],
+                 "fk1fl1.tiff")
+    
+    fit <- lm(AbsPositionZ~PositionX+PositionY,data=xi)
+    y <- split(xi,f=xi$Field)
+    
+    yi <- y[[1]]
+    x0 <- yi$PositionX[1]
+    y0 <- yi$PositionY[1]
+    
+    im <- expand.grid(PositionX=seq(0,dxy,length.out = nxy),
+                      PositionY=seq(0,dxy,length.out = nxy))
+    
+    
+    im$AbsPositionZ <- predict(fit,im)
+    
+    
+    im$plane <- pbapply::pbsapply(im$AbsPositionZ,function(z){
+      yi$Plane[which.min(abs(z-yi$AbsPositionZ))]
+    })
+    
+    m <- reshape2::acast(im,PositionX~-PositionY,value.var = "plane")/max(yi$Plane)
+    
+    saveRDS(m,paste0("/mnt/andor_lab/Jackson/Jackson_Operaphenix/240717_SUM159_MEMIC/sliceMaps2/",gsub(".tiff",".Rds",id)))
+    return(0)
+  },error=function(e) print(xi$URL[1]))
   
-  fit <- lm(AbsPositionZ~PositionX+PositionY,data=xi)
-  y <- split(xi,f=xi$Field)
-  
-  yi <- y[[1]]
-  x0 <- yi$PositionX[1]
-  y0 <- yi$PositionY[1]
-  
-  im <- expand.grid(PositionX=seq(0,dxy,length.out = nxy),
-                    PositionY=seq(0,dxy,length.out = nxy))
-  
-  
-  im$AbsPositionZ <- predict(fit,im)
-  
-  
-  im$plane <- pbapply::pbsapply(im$AbsPositionZ,function(z){
-    yi$Plane[which.min(abs(z-yi$AbsPositionZ))]
-  })
-  
-  m <- reshape2::acast(im,PositionX~-PositionY,value.var = "plane")/max(yi$Plane)
-  
-  saveRDS(m,paste0("/mnt/andor_lab/Jackson/Jackson_Operaphenix/240717_SUM159_MEMIC/sliceMaps2/",gsub(".tiff",".Rds",id)))
-  return(0)
 })
