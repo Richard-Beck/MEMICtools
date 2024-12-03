873639d695ab83d5b4c2e9f0b921559bdc33e104
= <=>
2024-12-03 16:39:06 -0500

    updated metadata analysis

--- scripts/make_object_images.R
+++ scripts/make_object_images.R
@@ -1,13 +1,25 @@
-x <- readRDS("/mnt/andor_lab/Jackson/Jackson_Operaphenix/240717_SUM159_MEMIC/trial_objects.Rds")
-#x <- readRDS("~/projects/017_jax/MEMICtools/data/trial_objects.Rds")
-nxy <- 2160
 
-x <- split(x,f=x$Field)
-library(tiff)
+target_data <- "/mnt/andor_lab/Jackson/Jackson_Operaphenix/240717_SUM159_MEMIC/objects.txt"
+x <- data.table::fread(target_data)
+#x <- readRDS("/mnt/andor_lab/Jackson/Jackson_Operaphenix/240717_SUM159_MEMIC/trial_objects.Rds")
+#x <- readRDS("~/projects/017_jax/MEMICtools/data/trial_objects.Rds")
+
+
+x <- split(x,f=interaction(x$Row,x$Col,x$Timepoint,x$Field))
+
+
+library(parallel)
+ncores <- 40
+cl <- makeCluster(getOption("cl.cores", ncores))
 outDir <- "/mnt/andor_lab/Jackson/Jackson_Operaphenix/240717_SUM159_MEMIC/objectImages/"
+
 dir.create(outDir)
-x <- x[order(sapply(x,nrow))]
-lapply(x,function(xi){
+
+print(paste("processing",length(x),"images..."))
+parLapplyLB(cl=cl,X=x,fun=function(xi){
+  library(tiff)
+  outDir <- "/mnt/andor_lab/Jackson/Jackson_Operaphenix/240717_SUM159_MEMIC/objectImages/"
+  nxy <- 2160
   x0 <- matrix(0,nxy,nxy)
   for(b in xi$`Bounding Box`){
     b <- as.numeric(unlist(strsplit(substr(b,2,nchar(b)-1),split="[,]")))
@@ -26,7 +38,7 @@ lapply(x,function(xi){
                "t",stringr::str_pad(xi$Timepoint[1],2,pad=0),
                ".tiff")
   
-  writeTIFF(x0,paste0(outDir,id),bits.per.sample = 16)
+  writeTIFF(x0,paste0(outDir,id),bits.per.sample = 8)
 })
 
 
