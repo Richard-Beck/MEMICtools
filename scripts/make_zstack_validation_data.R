bb2e3d80f7561d836d78d143349ac74378ee6bb2
= <=>
2024-12-04 15:29:38 -0500

    add validation script

---
+++ scripts/make_zstack_validation_data.R
@@ -0,0 +1,49 @@
+
+base_dir <- "/mnt/andor_lab/Jackson/Jackson_Operaphenix/240717_SUM159_MEMIC/"
+setwd(base_dir)
+
+output_dir <- "processed_images/zstack_validation/"
+dir.create(output_dir,recursive = T)
+input_dir <- "raw_images/"
+
+x <- readRDS("metadata.Rds")
+x <- x[x$Channel==2,]
+x <- split(x,f=interaction(x$Row,x$Col))
+
+lapply(x,function(xi){
+  x1 <- xi[xi$Timepoint==1,]
+  fields <- unique(x1$Field)
+  x1 <- x1[x1$Field==sample(fields,1),]
+  x1 <- x1[order(x1$Plane),]
+  tiff_files <- x1$URL
+  processed_images <- lapply(tiff_files,function(file){
+    image <- image_read(paste0(input_dir,file))
+    image <- image_resize(image, "1000x1000!")
+    image <- image_normalize(image)
+    image <- image_convert(image, depth = 8)
+    return(image)
+  })
+  multichannel_image <- image_join(processed_images)
+  saveAs <- paste0(output_dir,"r",xi$Row,"c",xi$Col,"f",xi$Field,"t",xi$Timepoint,".tiff")
+  image_write(multichannel_image, path = saveAs, format = "tiff")
+  
+  x1 <- xi[xi$Timepoint==10,]
+  fields <- unique(x1$Field)
+  x1 <- x1[x1$Field==sample(fields,1),]
+  x1 <- x1[order(x1$Plane),]
+  tiff_files <- x1$URL
+  
+  processed_images <- lapply(tiff_files,function(file){
+    image <- image_read(paste0(input_dir,file))
+    image <- image_resize(image, "1000x1000!")
+    image <- image_normalize(image)
+    image <- image_convert(image, depth = 8)
+    return(image)
+  })
+  multichannel_image <- image_join(processed_images)
+  saveAs <- paste0(output_dir,"r",xi$Row,"c",xi$Col,"f",xi$Field,"t",xi$Timepoint,".tiff")
+  image_write(multichannel_image, path = saveAs, format = "tiff")
+  
+})
+
+
